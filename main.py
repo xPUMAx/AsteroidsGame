a95c0bf61e7d1c8cc9aad885323941d599e20a85
PUMAx <cpope910@gmail.com>
2025-08-01 16:06:38 -0700

    Added player movement, asteroid & asteroid class fields

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import os
+os.environ['SDL_VIDEODRIVER_WINDOW_POS'] = '0, 0'  # initialize the window in the center
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()  # Initialize all imported pygame modules
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Create the game window
     pygame.display.set_caption("Asteroids Game")  # Set the window title
 
+    gameClock = pygame.time.Clock()
+    dt = 0  # Initialize delta time
+    updateable = pygame.sprite.Group()  # Initialize sprite group for updateable objects
+    drawable = pygame.sprite.Group()  # Initialize sprite group for drawable objects
+    asteroids = pygame.sprite.Group()  # Initialize sprite group for asteroids
+    Player.containers = (updateable, drawable)  # Set the containers for Player class
+    Asteroid.containers = (asteroids, updateable, drawable)  # Set the containers for Asteroid class
+    AsteroidField.containers = (updateable)  # Create an asteroid field instance
+
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)  # Create a player instance
+    asteroidfield = AsteroidField()  # Create an asteroid field instance
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))  # Fill the screen with black
-        pygame.display.flip()  # Update the display
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = gameClock.tick(60)/1000  # Limit the frame rate to 60 FPS and returns seconds to delta time
+        screen.fill((0, 0, 0))  # Fill the screen with black
+        
+        updateable.update(dt)  # Update the player
+        for object in drawable:
+            object.draw(screen)  # Draw objects on the screen
+
+        pygame.display.flip()  # Update the display   
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,40 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0  # Initialize rotation
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        points = self.triangle()
+        pygame.draw.polygon(screen, (255, 255, 255), points, 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
